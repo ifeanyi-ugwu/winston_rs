b708af8a6e9d362eb090ba5a3d4ecded08482555
ifeanyi-valentine <urielifeanyi@gmail.com>
2024-09-10 20:43:06 +0100

    fixed the log macros bug

--- src/logger/log_macros.rs
+++ src/logger/log_macros.rs
@@ -2,21 +2,21 @@
 #[macro_export]
 macro_rules! log_info {
     ($($arg:tt)*) => {
-        $crate::log(LogInfo::new("info", &format!($($arg)*)));
+        $crate::log($crate::format::LogInfo::new("info", &format!($($arg)*)));
     }
 }
 
 #[macro_export]
 macro_rules! log_warn {
     ($($arg:tt)*) => {
-        $crate::log(LogInfo::new("info", &format!($($arg)*)));
+        $crate::log($crate::format::LogInfo::new("warn", &format!($($arg)*)));
     }
 }
 
 #[macro_export]
 macro_rules! log_error {
     ($($arg:tt)*) => {
-$crate::log(LogInfo::new("info", &format!($($arg)*)));
+        $crate::log($crate::format::LogInfo::new("error", &format!($($arg)*)));
     }
 }
 
