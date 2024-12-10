469af4ba64ccaccc0a73792ddaaa9e3c43a61e8b
ifeanyi-valentine <urielifeanyi@gmail.com>
2024-12-10 14:44:18 +0100

    feat(transport): re-export WriterTransport and add stdout/stderr convenience functions

--- src/logger/transports/mod.rs
+++ src/logger/transports/mod.rs
@@ -3,3 +3,14 @@ pub mod file;
 
 pub use console::ConsoleTransport as Console;
 pub use file::FileTransport as File;
+use std::io;
+pub use winston_transport::WriterTransport;
+
+// Convenience functions
+pub fn stdout() -> WriterTransport<io::Stdout> {
+    WriterTransport::new(io::stdout())
+}
+
+pub fn stderr() -> WriterTransport<io::Stderr> {
+    WriterTransport::new(io::stderr())
+}
