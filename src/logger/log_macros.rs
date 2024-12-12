79f6b42fa48c68a6b83204e6acbf5d7a5db88610
ifeanyi-valentine <urielifeanyi@gmail.com>
2024-12-12 06:35:36 +0100

    feat!: remove redundant`log_info`, `log_warn` and `log_error` macros

--- src/logger/log_macros.rs
+++ src/logger/log_macros.rs
@@ -1,27 +1,3 @@
-// Convenience macros for global logging
-#[macro_export]
-macro_rules! log_info {
-    ($($arg:tt)*) => {
-        $crate::log($crate::format::LogInfo::new("info", &format!($($arg)*)));
-    }
-}
-
-#[macro_export]
-macro_rules! log_warn {
-    ($($arg:tt)*) => {
-        $crate::log($crate::format::LogInfo::new("warn", &format!($($arg)*)));
-    }
-}
-
-#[macro_export]
-macro_rules! log_error {
-    ($($arg:tt)*) => {
-        $crate::log($crate::format::LogInfo::new("error", &format!($($arg)*)));
-    }
-}
-
-// ... Add more macros for other log levels
-
 #[macro_export]
 macro_rules! log {
     ($level:ident, $message:expr $(, $key:ident = $value:expr)* $(,)?) => {{
